6cb838fbd74f03e76f26513c883b90372276e17a
Ondrej Jirman <megous@megous.com>
2020-10-29 02:15:56 +0100

    Simplify theme generator

--- theme/render.sh
+++ theme/render.sh
@@ -5,15 +5,20 @@ set -e -x
 rm -rf pngs argb
 mkdir -p pngs argb
 
-gcc -o make-argb argb.c
-
 for f in *.svg
 do
 	out=${f%.svg}
 	
 	inkscape -y 1 -b black --export-type=png -o pngs/$out.png $f
-	convert pngs/$out.png argb/$out.rgba
-	./make-argb argb/$out.rgba argb/$out.argb
-	rm argb/$out.rgba
+	convert pngs/$out.png argb/$out.bgra
+	mv argb/$out.bgra argb/$out.argb
 done
 
+for f in *.png
+do
+	out=${f%.png}
+	convert $out.png argb/$out.bgra
+	mv argb/$out.bgra argb/$out.argb
+done
+
+rm -rf pngs
